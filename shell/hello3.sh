ee29b6434e4a7b22df00271ee99955219e223c33
BiskupBoruta <73695340+BiskupBoruta@users.noreply.github.com>
2020-12-03 23:42:14 +0100

    challenge #25: hello world

---
+++ shell/hello3.sh
@@ -0,0 +1,8 @@
+# Challenge #25
+# https://www.codewars.com/kata/57036f007fd72e3b77000023
+
+# As functions in bash are pretty much compound commands, they always return
+# the exit status. As a matter of fact, the exit status is the only thing they
+# can return. This challenge does not really make sense in bash.
+
+printf "Hello World!"

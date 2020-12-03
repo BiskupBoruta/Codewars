f0bf892a595aba996ef604d340c7ff24b2da6caa
BiskupBoruta <73695340+BiskupBoruta@users.noreply.github.com>
2020-12-03 22:50:22 +0100

    challenge #21: hula hoop

---
+++ shell/hoop.sh
@@ -0,0 +1,5 @@
+# Challenge #21
+# https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+(($1 >= 10)) && printf "Great, now move on to tricks" || \
+                printf "Keep at it until you get it"
